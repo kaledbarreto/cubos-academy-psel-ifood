43f8428eda01bbcd1756c245eb3543313794fea5
Kaled Barreto <kaledbarreto@gmail.com>
2021-08-10 21:21:46 -0300

    Challenge 03

---
+++ Esconde-esconde.js
@@ -0,0 +1,11 @@
+function solucao(numero) {
+  var i = 21;
+if(numero <= 20){
+      console.log("VOCE CONSEGUE, MANINHO");
+  }else{
+      while(i<=numero){
+          console.log(i);
+          i++;
+      }
+  }
+}
